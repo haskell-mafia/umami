54afa9a3bf074a0a1ada4c0ecf7bc151b37203fc
Amos Robinson <amos.robinson@gmail.com>
2016-03-23 12:51:57 +1100

    Add some comments to the monad transformers

--- src/Umami/Monad/FixT.hs
+++ src/Umami/Monad/FixT.hs
@@ -1,4 +1,17 @@
 -- | Monad transformer for evaluating to a fixpoint
+-- The idea is that some transforms need to be run multiple times.
+-- Deciding whether to run a transform again can be somewhat tedious though,
+-- as you cannot necessarily just run some transform @f@ on @x@ until @f x == x@.
+--
+-- This might not be ideal for a few reasons:
+-- * @x@ might not implement @Eq@;
+-- * @x@ might implement @Eq@, but could contain floats of @NaN@, in which case @NaN /= NaN@; or
+-- * checking equality can be expensive.
+--
+-- Instead, this provides a function called @progress@, with the same type as @return@,
+-- that marks the current transform as having "made progress": that is, it should be re-run again.
+-- Then you can provide @fixpoint@ with a function of type @a -> FixT _ a@, which will be re-run
+-- until no progress is made.
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE BangPatterns      #-}
 module Umami.Monad.FixT (
@@ -14,15 +27,18 @@ import              P
 
 import              Control.Monad.Trans.Class
 
+-- | Fixpoint monad transformer.
 newtype FixT m a
  = FixT
  { runFixT :: m (a, Progress) }
 
+-- | Have we made progress?
 data Progress
  = RunAgain
  | NoProgress
  deriving (Eq, Ord, Show)
 
+-- | Apply the transform until it no longer makes progress
 fixpoint :: Monad m => (a -> FixT m a) -> a -> m a
 fixpoint f a
  = do (a',prog) <- runFixT $! f a
@@ -31,12 +47,14 @@ fixpoint f a
        NoProgress -> return a'
 {-# INLINE fixpoint #-}
 
+-- | Run a FixT once, regardless of whether it believes it makes progress or not
 once :: Monad m => FixT m a -> m a
 once f
  = do (r,_) <- runFixT f
       return r
 {-# INLINE once #-}
 
+-- | Take a function that returns @Just@ on progress and @Nothing@ on completion.
 fixOfMaybe :: Monad m => (a -> m (Maybe a)) -> a -> FixT m a
 fixOfMaybe f a
  = do a' <- lift $ f $! a
@@ -51,6 +69,7 @@ eitherProgress _ RunAgain = RunAgain
 eitherProgress _ _        = NoProgress
 {-# INLINE eitherProgress #-}
 
+-- | Return a value and proclaim: "it might be worth running again"
 progress :: Monad m => a -> FixT m a
 progress a
  = FixT $ return (a, RunAgain)
--- src/Umami/Monad/FreshT.hs
+++ src/Umami/Monad/FreshT.hs
@@ -1,13 +1,14 @@
--- | Monad transformer for evaluating to a fixpoint
+-- | Fresh variable plumbing:
+-- Monad transformer for supplying new variable names.
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE BangPatterns      #-}
 module Umami.Monad.FreshT (
-      FreshT   (..)
+      evalFreshT
+    , evalFresh
+    , FreshT   (..)
     , Fresh
     , FreshFun
     , fresh
-    , evalFreshT
-    , evalFresh
     ) where
 
 import              P
@@ -17,22 +18,32 @@ import              Control.Monad.Trans.Class
 import              Data.Functor.Identity
 
 
+-- | Evaluate fresh transformer
+-- Takes the action to perform, as well as a generator function and a starting number (probably zero)
 evalFreshT :: Functor m => FreshT n m a -> FreshFun n -> Int -> m a
 evalFreshT m fun st
  = fst <$> runFreshT m fun st
 
+-- | Evaluate fresh monad
 evalFresh :: Fresh n a -> FreshFun n -> Int -> a
 evalFresh m fun st
  = runIdentity $ evalFreshT m fun st
 
 
+-- | Fresh variable plumbing transformer
+-- We may like to change this to a StateT or an IORef at some stage.
 newtype FreshT n m a
  = FreshT
  { runFreshT :: FreshFun n -> Int -> m (a, Int) }
 
+-- | Fresh variable monad
 type Fresh n
  = FreshT n Identity
 
+-- | Generator function for fresh names.
+-- Takes an Int, which is the "variable number" for this step,
+-- and an optional name to base it on.
+-- The idea is that if a name is given, the new name can use this as a prefix.
 type FreshFun n
  = Int -> Maybe n -> n
 
@@ -45,6 +56,8 @@ instance Monad m => Monad (FreshT n m) where
 
  return a = FreshT $ \_ st -> return (a, st)
 
+-- | Generate a fresh name
+-- A name may be given as a hint of what to prefix the new name with.
 fresh :: Monad m => Maybe n -> FreshT n m n
 fresh n
  = FreshT
@@ -68,6 +81,4 @@ instance MonadTrans (FreshT n) where
   $ \_ st
   -> do v <- m
         return (v, st)
-
-
 
